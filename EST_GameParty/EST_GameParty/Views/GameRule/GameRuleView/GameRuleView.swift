8f51151c5fcc81043f7d20e129e18edf7ba96d84
paul3004 <gsk1040@gmail.com>
2025-02-21 13:12:26 +0900

    [GameRule}#주석축소

--- EST_GameParty/EST_GameParty/Views/GameRule/GameRuleView/GameRuleView.swift
+++ EST_GameParty/EST_GameParty/Views/GameRule/GameRuleView/GameRuleView.swift
@@ -6,30 +6,29 @@
 
 import SwiftUI
 
-// 게임 규칙을 보여주는 View 구조체 정의
+
 struct GameRuleView: View {
-    // 애니메이션 표시 여부를 저장하는 상태 변수
+    
     @State private var showAnimationView = false
     
     var body: some View {
         ZStack {
-            // 그라데이션 배경 설정
-            // 최상단에서 하단으로 파란색 계열의 그라데이션 적용
+            
+            
             LinearGradient(gradient: Gradient(colors: [Color(hex: "0026FD"), Color(hex: "311b92")]),
                          startPoint: .top,
                          endPoint: .bottom)
-                .ignoresSafeArea() // 안전 영역 무시하고 전체 화면 채우기
-            
+                .ignoresSafeArea()
             VStack {
-                // 애니메이션 표시 상태에 따른 조건부 렌더링
+                
                 if showAnimationView {
                     // 애니메이션 타이틀 표시
                     TitleAnimationView()
-                        .background(Color.white.opacity(0.1)) // 반투명 배경
-                        .cornerRadius(20) // 모서리 둥글게
+                        .background(Color.white.opacity(0.1))
+                        .cornerRadius(20) 
                         .padding()
                 } else {
-                    // 게임 시작 버튼
+                
                     Button(action: {
                         // 버튼 클릭시 부드러운 애니메이션과 함께 상태 변경
                         withAnimation(.easeInOut) {
@@ -58,10 +57,10 @@ struct GameRuleView: View {
                             
                             // 시작 버튼 디자인
                             HStack {
-                                // 재생 아이콘
+                                
                                 Image(systemName: "play.circle.fill")
                                     .font(.title2)
-                                // 시작 텍스트
+                                
                                 Text("터치하여 시작")
                                     .fontWeight(.bold)
                             }
@@ -69,7 +68,7 @@ struct GameRuleView: View {
                             .padding(.horizontal, 30)
                             .padding(.vertical, 15)
                             .background(
-                                // 버튼 배경 그라데이션
+                                
                                 LinearGradient(
                                     colors: [Color(hex: "7c4dff"), Color(hex: "651fff")],
                                     startPoint: .leading,
